b04d380423feeb84961c84f15b00e8773d89dd72
tesla1856 <real.tesla@gmail.com>
2022-08-03 12:59:27 +0000

    восстановление джобов после перезапуска

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import os
 from threading import Thread
 
 from telegram import Update  #upm package(python-telegram-bot)
-from telegram.ext import Updater, CommandHandler, CallbackContext  #upm package(python-telegram-bot)
+from telegram.ext import Updater, CommandHandler, CallbackContext, JobQueue  #upm package(python-telegram-bot)
 
 from datetime import time, datetime, timedelta
 from pytz import timezone
 
 from flask import render_template
 from flask import Flask
+
+from replit import db
 
 import requests
 
@@ -25,8 +27,8 @@ BOT_START_DATE = datetime.now(TZ)
 
 @app.route('/')
 def home(page=None):
-    print("check: " + datetime.now(TZ).strftime("%d.%m.%y %H:%M") + " -> " +
-          BOT_START_DATE.strftime("%d.%m.%y %H:%M"))
+    #print("check: " + datetime.now(TZ).strftime("%d.%m.%y %H:%M") + " -> " +
+    #      BOT_START_DATE.strftime("%d.%m.%y %H:%M"))
     return render_template('home.html',
                            start_dt=BOT_START_DATE.strftime("%d.%m.%y %H:%M"))
 
@@ -37,13 +39,14 @@ def help_command(update: Update, context: CallbackContext) -> None:
     )
 
 
-def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+def remove_job_if_exists(name: str, job_queue: JobQueue) -> bool:
     """Remove job with given name. Returns whether job was removed."""
-    current_jobs = context.job_queue.get_jobs_by_name(name)
+    current_jobs = job_queue.get_jobs_by_name(name)
     if not current_jobs:
         return False
     for job in current_jobs:
         job.schedule_removal()
+        del db["job:" + job.name]
     return True
 
 
@@ -51,35 +54,45 @@ def alarm(context: CallbackContext) -> None:
     ctx = context.job.context
     job_name = ctx['job_name']
     chat_id = ctx['chat_id']
-    due = create_job(context, job_name, chat_id)
+    if 'eda' in job_name:
+        del db[f"job:{job_name}"]
+        due = create_job_eda(context.job_queue, job_name, chat_id)
+        msg = '🥣 Пора есть, следующее напоминание: ' + due.strftime("%H:%M")
+
     anek = ""
     try:
         resp = requests.get('http://rzhunemogu.ru/RandJSON.aspx?CType=1')
         anek = resp.text.replace('{"content":"', '').replace('"}', '')
     except:
         None
-    msg = '🥣 Пора есть, следующее напоминание: ' + due.strftime("%H:%M")
     if anek:
         msg += f'\n\n===\n{anek}'
-    print(msg)
+
     context.bot.send_message(chat_id, msg)
 
 
-def create_job(context: CallbackContext, job_name: str,
-               chat_id: int) -> datetime:
-    remove_job_if_exists(job_name, context)
-    now = datetime.now(TZ)
-    due = now + timedelta(seconds=EDA_PERIOD_SEC)
-    if due > TZ.localize(datetime.combine(now, EDA_END_TIME_MSK)):
-        due = TZ.localize(
-            datetime.combine(now + timedelta(days=1), EDA_START_TIME_MSK))
-    context.job_queue.run_once(alarm,
-                               due,
-                               name=job_name,
-                               context={
-                                   'chat_id': chat_id,
-                                   'job_name': job_name
-                               })
+def create_job_eda(job_queue: JobQueue, job_name: str,
+                   chat_id: int, due: datetime = None) -> datetime:
+    remove_job_if_exists(job_name, job_queue)
+    if not due:
+        now = datetime.now(TZ)
+        due = now + timedelta(seconds=EDA_PERIOD_SEC)
+        if due > TZ.localize(datetime.combine(now, EDA_END_TIME_MSK)):
+            due = TZ.localize(
+                datetime.combine(now + timedelta(days=1), EDA_START_TIME_MSK))
+    # print({'due': due.isoformat(), 'chat_id': chat_id, 'job_name': job_name})
+    job_queue.run_once(alarm,
+                       due,
+                       name=job_name,
+                       context={
+                           'chat_id': chat_id,
+                           'job_name': job_name
+                       })
+    db[f"job:{job_name}"] = {
+        'due': datetime.strftime(due,"%d%m%y%H%M%S"),
+        'chat_id': chat_id,
+        'job_name': job_name
+    }
     return due
 
 
@@ -97,7 +110,7 @@ def set_eda_timer(update: Update,
         timer_list(update, context, job_name)
 
     else:
-        due = create_job(context, job_name, chat_id)
+        due = create_job_eda(context.job_queue, job_name, chat_id)
         update.effective_message.reply_text(
             f'Напоминание установлено на {due.strftime("%H:%M")}')
 
@@ -105,7 +118,7 @@ def set_eda_timer(update: Update,
 def unset(update: Update, context: CallbackContext, job_name: str) -> None:
     """Remove the job if the user changed their mind."""
     chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(job_name, context)
+    job_removed = remove_job_if_exists(job_name, context.job_queue)
     text = "Напоминания отменены!" if job_removed else "Никаких напоминаний и не было запланировано."
     context.bot.send_message(chat_id, text)
 
@@ -124,7 +137,15 @@ def run():
     app.run(host='0.0.0.0', port=8080)
 
 
+def jobs_up_from_db(job_queue: JobQueue):
+    for d in db.prefix("job:"):
+        if 'eda' in db[d]["job_name"]:
+            print("start job ", db[d])
+            create_job_eda(job_queue, db[d]["job_name"], db[d]["chat_id"], TZ.localize(datetime.strptime(db[d]["due"],"%d%m%y%H%M%S")))
+
+
 def main():
+    print("Start")
     updater = Updater(os.getenv("TOKEN"))
 
     dispatcher = updater.dispatcher
@@ -133,6 +154,7 @@ def main():
     dispatcher.add_handler(CommandHandler("eda", set_eda_timer))
 
     #dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), log))
+    jobs_up_from_db(updater.job_queue)
 
     updater.start_polling()
 
